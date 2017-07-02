037948ff90baf14b72223858ba0a487585ae7fee
Ethan Och <ochxx013@umn.edu>
2017-07-02 12:58:58 -0500

    Version 2
    Added support for status indicator lights.

--- GPS_LOGGER.ino
+++ GPS_LOGGER.ino
@@ -9,16 +9,17 @@
 #define MAX_LOG_FILES 100
 #define LOG_COLUMN_COUNT 8
 #define LOG_RATE 1000
-#define GPS_BAUD 115200
-#define SERIAL_BAUD 115200
+#define GPS_BAUD 9600
+#define SERIAL_BAUD 9600
 #define ARDUINO_GPS_RX 9
 #define ARDUINO_GPS_TX 8
 #define GPS_PORT SS_GPS
 #define SERIAL_MONITOR Serial
+#define LED1 5
+#define LED2 6
 
 char LOG_FILE_NAME[13];
 char * LOG_COLUMN_NAMES[LOG_COLUMN_COUNT] = {"LATITUDE", "LONGITUDE", "ALTITUDE", "VELOCITY", "HEADING", "DATE", "TIME", "SATELLITES"};
-unsigned long LAST_LOG = 0;
 TinyGPSPlus TINY_GPS;
 SoftwareSerial SS_GPS(ARDUINO_GPS_TX, ARDUINO_GPS_RX);
 
@@ -26,6 +27,11 @@ void setup()
 {
   SERIAL_MONITOR.begin(SERIAL_BAUD);
   GPS_PORT.begin(GPS_BAUD);
+
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
 
   SERIAL_MONITOR.println("+------------------------------------+");
   SERIAL_MONITOR.println("|   TinyGPS++ Improved Data Logger   |");
@@ -54,45 +60,75 @@ void setup()
   PRINT_HEADER();
     
   SERIAL_MONITOR.println("*** STARTING DATALOGGING ***");
+
+  for (int i = 0; i < 5; i++)
+  {
+    digitalWrite(LED2, LOW);
+    delay(50);
+    digitalWrite(LED2, HIGH);
+    delay(50);
+    digitalWrite(LED1, LOW);
+    delay(50);
+    digitalWrite(LED1, HIGH);
+    delay(50);
+  }
 }
 
 void loop()
 {
-  if ((LAST_LOG + LOG_RATE) <= millis())
+  int light_on = 5, light_off = LOG_RATE - light_on;
+  digitalWrite(LED2, LOW);
+  
+  if (TINY_GPS.satellites.value() > 3)
   {
-    if (TINY_GPS.location.isUpdated())
+    SERIAL_MONITOR.print("[");
+    SERIAL_MONITOR.print(TINY_GPS.date.value());
+    SERIAL_MONITOR.print("][");
+    SERIAL_MONITOR.print(TINY_GPS.time.value());
+    SERIAL_MONITOR.print("] ");
+    
+    if (LOG_GPS_DATA())
     {
-      SERIAL_MONITOR.print("[");
-      SERIAL_MONITOR.print(TINY_GPS.date.value());
-      SERIAL_MONITOR.print("][");
-      SERIAL_MONITOR.print(TINY_GPS.time.value());
-      SERIAL_MONITOR.print("] ");
+      digitalWrite(LED1, LOW);
       
-      if (LOG_GPS_DATA())
-      {
-        SERIAL_MONITOR.println("GPS data logged.");
-        LAST_LOG = millis();
-      }
-      else
-      {
-        SERIAL_MONITOR.println("Failed to log new GPS data.");
-      }
+      SERIAL_MONITOR.println("GPS data logged.");
     }
     else
     {
-      SERIAL_MONITOR.print("[");
-      SERIAL_MONITOR.print(TINY_GPS.date.value());
-      SERIAL_MONITOR.print("][");
-      SERIAL_MONITOR.print(TINY_GPS.time.value());
-      SERIAL_MONITOR.print("] No GPS data found. Number of satellite connections: ");
-      SERIAL_MONITOR.println(TINY_GPS.satellites.value());
+      SERIAL_MONITOR.println("Failed to log new GPS data.");
     }
   }
+  else
+  {
+    SERIAL_MONITOR.println("Insufficient satellites to receive reliable GPS data.");
 
+    while (TINY_GPS.satellites.value() < 4)
+    {
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED1, LOW);
+      delay(500);
+      TINY_GPS.encode(GPS_PORT.read());
+      digitalWrite(LED2, LOW);
+      delay(500);
+      TINY_GPS.encode(GPS_PORT.read());
+      digitalWrite(LED1, HIGH);
+      delay(500);
+      TINY_GPS.encode(GPS_PORT.read());
+      digitalWrite(LED1, LOW);
+      delay(500);
+      TINY_GPS.encode(GPS_PORT.read());
+    }
+  }
+  
   while (GPS_PORT.available())
   {
     TINY_GPS.encode(GPS_PORT.read());
   }
+  
+  delay(light_on);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  delay(light_off);
 }
 
 byte LOG_GPS_DATA()
@@ -105,9 +141,9 @@ byte LOG_GPS_DATA()
     LOG_FILE.print(',');
     LOG_FILE.print(TINY_GPS.location.lng(), 6);
     LOG_FILE.print(',');
-    LOG_FILE.print(TINY_GPS.altitude.feet(), 1);
+    LOG_FILE.print(TINY_GPS.altitude.meters(), 1);
     LOG_FILE.print(',');
-    LOG_FILE.print(TINY_GPS.speed.mph(), 1);
+    LOG_FILE.print(TINY_GPS.speed.mps(), 1);
     LOG_FILE.print(',');
     LOG_FILE.print(TINY_GPS.course.deg(), 1);
     LOG_FILE.print(',');
@@ -180,4 +216,3 @@ void UPDATE_FILE_NAME()
   SERIAL_MONITOR.println(LOG_FILE_NAME);
   SERIAL_MONITOR.println();
 }
-
